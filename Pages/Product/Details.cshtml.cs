814227367321bb34e4f732e018dc21e650dea110
Jame Tabrita <James.tabrita@yahoo.com>
2020-03-27 01:59:43 +0100

    Details Page

---
+++ Pages/Product/Details.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorCRUDApp.Repository;
+
+namespace RazorCRUDApp.Pages.Product
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly IProductRepository _productRepository;
+
+        public DetailsModel(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        }
+
+        public Entities.Product product { get; set; }
+        
+        public async Task<IActionResult> OnGetAsync(int productId)
+        {
+            
+            if (productId == 0)
+            {
+                return RedirectToPage("./Index");
+            }
+            product = await _productRepository.GetProductByIdAsync(productId);
+            return Page();
+        }
+    }
+}
